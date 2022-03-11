a0d79e59be3dd4de3af658c704087d4a97816b2d
Thomas Godwin <leddfoot36@gmail.com>
2022-03-11 15:38:13 +0100

    corrected all links entry page. styled header

--- src/mainEntry/PortfolioSection.js
+++ src/mainEntry/PortfolioSection.js
@@ -26,16 +26,18 @@ function PortfolioSection() {
         <div className="portfolio-section">
           <h1>Oh SH!T! is my vinmonopolet still open?!?</h1>
           <div className="image-container">
-            <img
-              className="vinmonopolet--img"
-              src={vinmonpolet}
-              alt="view of vinmopolet page"
-            />
-            <img
-              src={vinmonpoletMobile}
-              className="mobile vinmonopolet--img"
-              alt="view of mobile vinmopolet page"
-            />
+          <a href="https://leddfoot-test.herokuapp.com/">
+          <img
+          className="vinmonopolet--img"
+          src={vinmonpolet}
+          alt="view of vinmopolet page"
+        />
+        <img
+          src={vinmonpoletMobile}
+          className="mobile vinmonopolet--img"
+          alt="view of mobile vinmopolet page"
+        />
+          </a>
           </div>
 
           <div className="portfolio__description">
@@ -54,12 +56,14 @@ function PortfolioSection() {
           <h1>Easy Cors Fix Documentation Page</h1>
 
           <div className="image-container">
-            <img src={docs} alt="view of easy cors fix documentation page" />
-            <img
-              src={docsMobile}
-              className="mobile"
-              alt="view of easy cors fix documentation mobile page"
-            />
+          <a href="https://fcc-docs.herokuapp.com/">
+          <img src={docs} alt="view of easy cors fix documentation page" />
+          <img
+            src={docsMobile}
+            className="mobile"
+            alt="view of easy cors fix documentation mobile page"
+          />
+          </a>
           </div>
 
           <div className="portfolio__description">
@@ -75,12 +79,14 @@ function PortfolioSection() {
           <h1>Product Landing Page</h1>
 
           <div className="image-container">
-            <img src={landingPage} alt="view of easy cors fix documentation page" />
-            <img
-              src={landingPageMobile}
-              className="mobile"
-              alt="view of easy cors fix documentation mobile page"
-            />
+          <a href="/landing-page">
+          <img src={landingPage} alt="view of easy cors fix documentation page" />
+          <img
+            src={landingPageMobile}
+            className="mobile"
+            alt="view of easy cors fix documentation mobile page"
+          />
+          </a>
           </div>
 
           <div className="portfolio__description">
@@ -88,7 +94,7 @@ function PortfolioSection() {
             <p>
              This project was created for a certification on FreeCodeCamp. It is pretty straightforward product landing page. The company is fictional and doesn't really exist. Shows off a nice design and solid mobile first styling.
             </p>
-            <p>The code repository for the product landing page project is on <a href="xx">Github</a>. It is deployed live at Heroku <a href="/landing-page">here</a></p>
+            <p>The code repository for the product landing page project is on <a href="https://github.com/Leddfoot/landing_page_fcc">Github</a>. It is deployed live at Heroku <a href="/landing-page">here</a></p>
           </div>
         </div>
 
@@ -96,12 +102,14 @@ function PortfolioSection() {
           <h1>Product Survey Page</h1>
 
           <div className="image-container">
+            <a href="/survey">
             <img src={survey} alt="view of survey page" />
             <img
               src={surveyMobile}
               className="mobile"
               alt="view of survey mobile page"
             />
+            </a>
           </div>
 
           <div className="portfolio__description">
@@ -109,7 +117,7 @@ function PortfolioSection() {
             <p>
             This project was created for a certification on FreeCodeCamp. It is a survey page that complements the product landing page above. Note that there is no functionality. It is more of a visual implementation project. Note that most of the styling uses id selectors. I did that because of their high specificity. I have since taken a CSS deep dive and use more efficient class selectors now.
             </p> 
-            <p>The code repository for the product survey project is on <a href="xx">Github</a>. It is deployed live at Heroku <a href="/survey">here</a></p>
+            <p>The code repository for the product survey project is on <a href="https://github.com/Leddfoot/survey-form-fcc">Github</a>. It is deployed live at Heroku <a href="/survey">here</a></p>
           </div>
         </div>
 
@@ -117,12 +125,14 @@ function PortfolioSection() {
           <h1>Pomodoro Timer</h1>
 
           <div className="image-container">
+            <a href="/pomodoro">
             <img src={pomodoro} alt="view of pomodoro page" />
             <img
               src={pomodoroMobile}
               className="mobile"
               alt="view of pomodoro mobile page"
             />
+            </a>
           </div>
 
           <div className="portfolio__description">
@@ -131,7 +141,7 @@ function PortfolioSection() {
             <p>
               This is yet another certification requirement for FreeCodeCamp. The tests involving timing were a little tricky to satisfy. Designing a hook for the timer was difficult. Apparently I am not the only person on the planet that found that to be difficult. Just note that some of the setinterval hook comes from <a href="https://overreacted.io/making-setinterval-declarative-with-react-hooks/">this example</a> on the net where Dan Abramov explains how to implement it.
             </p>
-            <p>The code repository for the pomodoro project is on <a href="xx">Github</a>. It is deployed live at Heroku <a href="/pomodoro">here</a></p>
+            <p>The code repository for the pomodoro project is on <a href="https://github.com/Leddfoot/pomodoro_fcc">Github</a>. It is deployed live at Heroku <a href="/pomodoro">here</a></p>
           </div>
         </div>
 
@@ -139,12 +149,14 @@ function PortfolioSection() {
           <h1>Quote Fetcher</h1>
 
           <div className="image-container">
-            <img src={quote} alt="view of quote page" />
-            <img
-              src={quoteMobile}
-              className="mobile"
-              alt="view of quote mobile page"
-            />
+              <a href="/quote">
+              <img src={quote} alt="view of quote page" />
+              <img
+                src={quoteMobile}
+                className="mobile"
+                alt="view of quote mobile page"
+              />
+              </a>
           </div>
 
           <div className="portfolio__description">
@@ -161,12 +173,14 @@ function PortfolioSection() {
           <h1>Drum Machine</h1>
 
           <div className="image-container">
+            <a href="/drum">
             <img className="drum--img" src={drum} alt="view of drum page" />
             <img
               src={drumMobile}
               className="mobile drum--img"
               alt="view of drum mobile page"
             />
+            </a>
           </div>
 
           <div className="portfolio__description">
